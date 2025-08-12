98af90203cf3c7d94e31920239a542538a750c87
Taiwo Owoseje <taiwoowoseje@gmail.com>
2025-08-12 15:37:01 +0100

    Added a command line to Tutorial 1

--- Week_1/Tutorial_1.py
+++ Week_1/Tutorial_1.py
@@ -1,8 +1,11 @@
-print("hello world, I am learning Python")
-print("i am a foremost ai developer")
-name = "Taiwo"
-print(name)
-print("My name is "+ name)
-print("My name is" + " " + name)
-print(f"hello {name}")
+# print("hello world, I am learning Python")
+# print("i am a foremost ai developer")
+# name = "Taiwo"
+# print(name)
+# print("My name is "+ name)
+# print("My name is" + " " + name)
+# print(f"hello {name}")
 
+name = "Olobe"
+gate = "loloko"
+print(name + gate)
